90d41a3bfdf919b4a22caf8189efbd3f1a8bf52c
Aline Buchino <aline.buchino@hotmail.com>
2022-10-29 22:55:41 -0300

    criando projeto

---
+++ src/test/java/io/github/com/br/apitestesunitarios/ApiTestesUnitariosApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.com.br.apitestesunitarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTestesUnitariosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
