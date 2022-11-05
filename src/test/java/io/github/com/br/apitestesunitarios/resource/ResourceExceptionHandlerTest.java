c2983c01aa70e7a88a32433cc79b2b844f64cdc6
Aline Buchino <aline.buchino@hotmail.com>
2022-11-05 18:50:06 -0300

    teste de exceção na classe ExceptionHandler da camada resource

---
+++ src/test/java/io/github/com/br/apitestesunitarios/resource/ResourceExceptionHandlerTest.java
@@ -0,0 +1,43 @@
+package io.github.com.br.apitestesunitarios.resource;
+
+import io.github.com.br.apitestesunitarios.resource.exceptions.ResourceExceptionHandler;
+import io.github.com.br.apitestesunitarios.resource.exceptions.StandardError;
+import io.github.com.br.apitestesunitarios.services.exceptions.ObjectNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+public class ResourceExceptionHandlerTest {
+
+    @InjectMocks
+    private ResourceExceptionHandler exceptionHandler;
+
+    @BeforeEach
+    void setUp(){
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void whenObjectNotFoundThenReturnResponseEntity(){
+        ResponseEntity<StandardError> response = exceptionHandler
+                .objectNotFound(new ObjectNotFoundException("Objeto não encontrado!"),
+                        new MockHttpServletRequest());
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(StandardError.class, response.getBody().getClass());
+        assertEquals("Objeto não encontrado!", response.getBody().getError());
+        assertEquals(404, response.getBody().getStatus());
+    }
+}
